e0b4ab4d753f0a13614bd9035f58d7edb1e96fc9
Shahin Alom <shahinalomshuvo94@gmail.com>
2024-01-05 15:54:48 +0600

    create app & make ready for work

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,33 @@
+import Lottie from "lottie-react";
+import animation from "../../assets/error.json";
+import { Link, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <div
+      className='flex flex-col justify-center items-center h-screen'
+      id='error-page'
+    >
+      <div className='w-96'>
+        <Lottie animationData={animation}></Lottie>
+      </div>
+      <div className='text-center text-2xl space-y-2'>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+        <Link
+          to={"/"}
+          className='btn bg-[#FF7C08] text-white border border-transparent hover:border-[#FF7C08] hover:bg-transparent hover:text-[#FF7C08] capitalize py-2 px-4 rounded-md transition duration-300 ease-in-out'
+        >
+          Go Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>welcome home</h2>
+    </div>
+  );
+};
+
+export default Home;
